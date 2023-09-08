ae0e6d7377d9b2e4a5258fcb0a549647284d9e8b
GadeRushi0202 <gaderushi0202@gmail.com>
2023-09-08 11:44:49 -0700

    Greater Number

---
+++ Code/Condition/Greater_Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Condition
+{
+    internal class Greater_Number
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 10;
+
+            if (a > b)
+            {
+                Console.WriteLine("A Number Greater Number");
+            }
+            else
+            {
+                Console.WriteLine("B Number Greater Number");
+            }
+        }
+    }
+}
