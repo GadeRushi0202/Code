d4d2e4abf802a00d6926bfc42170fd534f082a77
GadeRushi0202 <gaderushi0202@gmail.com>
2023-09-08 11:39:22 -0700

    Add project files.

---
+++ Code/Condition/First.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Condition
+{
+    internal class First
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a > 18)
+            {
+                Console.WriteLine("Vote");
+            }
+            else
+            {
+                Console.WriteLine("Not Vot");
+            }
+        }
+    }
+}
